99c95cac6dc9f7db9821414e0e19897186457a95
sentuh <142236429+sentuhxd@users.noreply.github.com>
2023-09-25 15:33:38 -0400

    fixed values in seeds sql

--- db/seeds.sql
+++ db/seeds.sql
@@ -9,19 +9,20 @@ VALUES
 
 INSERT INTO roles (title, salary, department_id)
 VALUES
-    ("Lead Sales", 100000, 2), 
-    ("Associate Sales", 80000, 2), 
-    ("Lead Engineer", 150000, 1), 
-    ("Engineer", 120000, 1), 
-    ("Accountant", 125000, 4), 
-    ("Head Lawyer", 250000, 3), 
-    ("Associate Lawyer", 190000, 3);
+    ("Lead Sales", 150000, 2), 
+    ("Associate Sales", 90000, 2), 
+    ("Lead Engineer", 250000, 1), 
+    ("Engineer", 175000, 1), 
+    ("Accountant", 200000, 4), 
+    ("Head Lawyer", 500000, 3), 
+    ("Associate Lawyer", 215000, 3);
 
 INSERT INTO employees (first_name, last_name, role_id)
-    ("Ian", "Rosey", 1)
-    ("Cool", "Person", 4)
-    ("Rinse", "Repeat", 2)
-    ("Ben", "Wright", 3)
-    ("Life", "Isgreat", 5)
-    ("Wink", "Yface", 6)
-    ("Rice", "Famer", 7)
+VALUES
+    ("Ian", "Rosey", 1),
+    ("Cool", "Person", 4),
+    ("Rinse", "Repeat", 2),
+    ("Ben", "Wright", 3),
+    ("Life", "Isgreat", 5),
+    ("Wink", "Yface", 6),
+    ("Rice", "Famer", 7);
