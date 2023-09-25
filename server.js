78fcdf39ac1f31de360d3a08e4ccf1028ea7bfac
sentuh <142236429+sentuhxd@users.noreply.github.com>
2023-09-25 15:20:11 -0400

    added an inquirer prompt to the serverjs file

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const mysql = require("mysql2");
 const inquirer = require('inquirer');
 const cTable = require('console.table');
+const { response } = require("express");
 
 const connection = mysql.createConnection({
   host: 'localhost',
@@ -10,21 +11,56 @@ const connection = mysql.createConnection({
   database: 'trackingemployees_db',
 })
 
-console.log("db connected");
-// DATA
-
-// APP/PORT
-
-
-// MIDDLEWARES
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-app.use(express.static("public"));
+connection.connect(function (err) {
+    if (err) throw err;
+    promptUser();
+});
 
 
+function promptUser(){
+    inquirer.prompt([
+        {
+            type: 'list',
+            name: 'overview',
+            message: 'What would you like to do in the employee tracker program?',
+            choices: ['view departments', 'view roles', 'view employees', 'add department', 'add roles', 'add employees', 'RETURN']
+        }
+    ]).then((response) => {
+        switch (response.overview) {
+            case 'view departments':
+                viewDepartments();
+                break;
+            
+            case 'view roles':
+                viewRoles();
+                break;
+            
+            case 'view employees':
+                viewEmployees();
+                break;
 
-// START THE SERVER
-app.listen(PORT, () =>
-  console.log(`Server running on http://localhost:${PORT}`)
-);
+            case 'add department':
+                addDepartment();
+                break;
+            
+            case 'add roles':
+                addRoles();
+                break;
+
+            case 'add employees':
+                addEmployees();
+                break;
+            
+            case 'RETURN':
+                connection.end();
+                console.log('\n You have exited the employee tracker \n')
+                return;
+            default:
+                break;
+
+        }
+    })
+}
+
+
+
